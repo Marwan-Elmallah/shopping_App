35f16d91bd4d3d5bc9563fc9c9c5075ec36b027e
Marwan-Elmallah <engmarwan0110@gmail.com>
2024-06-08 19:44:50 +0300

    add cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express")
 const { dbConnection } = require("./database")
 const app = express()
 const allRouter = require("./routes")
+const cors = require('cors')
+
 require('dotenv').config()
 
 
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
