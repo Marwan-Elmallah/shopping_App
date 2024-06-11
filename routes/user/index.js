ee85c913167229b23ca71c85f67fdff7ab7f519c
Marwan-Elmallah <engmarwan0110@gmail.com>
2024-06-12 00:42:15 +0300

    add multer and deleteRoute

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,6 +3,7 @@ const { UserController } = require('../../controller')
 
 
 router.get("/", UserController.getUser)
+router.get("/:id", UserController.getUser)
 router.post("/", UserController.register)
 router.post("/login", UserController.login)
 router.delete("/:id", UserController.deleteUser)
