94da8323b48a25b6647a819cf2e5f4e56dfa7ec0
Marwan-Elmallah <engmarwan0110@gmail.com>
2024-06-12 00:45:28 +0300

    updates

--- routes/product/index.js
+++ routes/product/index.js
@@ -4,6 +4,7 @@ const { uploadImage } = require('../../middleware/uploadImage')
 
 
 router.get("/", ProductController.getProduct)
+router.get("/:id", ProductController.getProduct)
 router.post("/", ProductController.createProduct)
 router.put("/:id", ProductController.updateProduct)
 router.delete("/:id", ProductController.deleteProduct)
